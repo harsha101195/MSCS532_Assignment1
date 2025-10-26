8ab380c8682dbce83554e088dc6783f1b1401d44
harsha101195 <st3364@columbia.edu>
2025-10-26 01:37:12 -0700

    adding code for insertion sort that sorts in decreasing order

--- InsertionSort.py
+++ InsertionSort.py
@@ -2,3 +2,20 @@
 This is an implementation of the Insertion Sort algorithm
 that sorts in monotonically decreasing order.
 """
+def insertionSort(arr):
+    l = len(arr)
+    
+    for i in range(1,l):
+        key = arr[i]
+
+        j = i-1
+        while j>=0 and key > arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = key
+
+    return arr
+
+print(insertionSort([3,5,1,6,7]))
+            
